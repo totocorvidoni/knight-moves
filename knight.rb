db5f4579d744bc736586b3fd0de3abfd632fb8c0
Toto Corvidoni <TotoCorvidoni@gmail.com>
2018-09-14 17:09:51 -0300

    git initiate

---
+++ knight.rb
@@ -0,0 +1,43 @@
+class KnightMovement
+  attr_accessor :position, :parent, :children
+  def initialize(position, parent = nil)
+    @position = position
+    @parent = parent
+    @children = []
+  end
+
+  def move
+    possible_moves = []
+    moves = [position, [2, 1]], [position, [2, -1]], [position, [-2, 1]], [position, [-2, -1]], [position, [1, 2]], [position, [1, -2]], [position, [-1, 2]], [position, [-1, -2]]
+    moves.each do |move|
+      square = move.transpose.map(&:sum)
+      possible_moves << square if square.all?(1..8)
+    end
+    possible_moves.each do |move|
+      new_move = KnightMovement.new(move, self)
+      children << new_move
+    end
+  end
+end
+
+def knight_moves(start, destination)
+  knight = KnightMovement.new(start)
+  queue = [knight]
+  path = []
+  loop do
+    if queue[0].position == destination
+      path << queue[0]
+      break
+    else
+      queue[0].move
+      queue[0].children.each { |movement| queue << movement }
+      queue.shift
+    end
+  end
+  until path[0] == knight
+    path.unshift path[0].parent
+  end
+  puts "To get from #{start} to #{destination} with your knight, you need to take this path:"
+  print path.map(&:position)
+  puts
+end
---
+++ test.rb
@@ -0,0 +1,13 @@
+class Test
+
+  def tast
+    tast
+  end
+
+  def self.test
+    puts 'hey, im working.'
+  end
+end
+
+a = Test.new
+a.tast
